f95637e659f92d57311fe082c59fc55b7050b0aa
alexandre824 <50344569+alexandre824@users.noreply.github.com>
2022-01-26 12:28:11 -0300

    Mudança de Header

--- sistem/index.php
+++ sistem/index.php
@@ -1,17 +1,18 @@
 <?php
 
-  // A sessão precisa ser iniciada em cada página diferente
-  if (!isset($_SESSION)) session_start();
+// A sessão precisa ser iniciada em cada página diferente
+if (!isset($_SESSION)) session_start();
 
-  // Verifica se não há a variável da sessão que identifica o usuário
-  if (!isset($_SESSION['UsuarioID'])) {
-      // Destrói a sessão por segurança
-      session_destroy();
-      // Redireciona o visitante de volta pro login
-      header("Location: ../index.html"); exit;
-  }
+// Verifica se não há a variável da sessão que identifica o usuário
+if (!isset($_SESSION['UsuarioID'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+    // Redireciona o visitante de volta pro login
+    header("Location: ../index.php");
+    exit;
+}
 
-  ?>
+?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
@@ -24,80 +25,121 @@
 </head>
 
 <body>
+    <header>
         <div class="container">
-          <div class="jumbotron">
-          <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
+            <nav class="navbar navbar-expand-lg ftco_navbar ftco-navbar-dark" id="ftco-navbar">
+                <div class="container">
+                    <a class="navbar-brand" href="index.html">Digital</a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
+                        aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="fa fa-bars"></span>
+                        Menu
+                    </button>
+                    <div class="collapse navbar-collapse" id="ftco-nav">
+                        <ul class="navbar-nav ml-auto mr-md-3">
+                            <li class="nav-item active">
+                                <a href="#" class="nav-link">Pendentes</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="entregues.php" class="nav-link">Entregues</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../users/index.php" class="nav-link">Usuario</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="../logout.php" class="nav-link">Logout</a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">Configurações gerais</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+            </nav>
+            <!-- END nav -->
+        </div>
+    </header>
+    <div class="container">
+        <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
+
+        <!-- <div class="jumbotron">
+            <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
 
             <div class="row">
                 <h2>SAA - Controle de Maquinas</h2>
             </div>
-          </div>
-            </br>
-            <div class="row">
-                <p>
-                    <a href="create.php" class="btn btn-success">Adicionar</a>
-                </p>
-                <br>
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">Id</th>
-                            <!-- <th scope="col">Nome</th> -->
-                            <th scope="col">Local</th>
-                            <th scope="col">Setor</th>
-                            <th scope="col">Entrada</th>
-                            <th scope="col">Status</th>
-                            <th scope="col">Chapa</th>
-                            <th scope="col">Chamado</th>
-                            <th scope="col">Ação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
-                        include 'banco.php';
-                        $pdo = Banco::conectar();
-                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
 
-                        foreach($pdo->query($sql)as $row)
-                        {
+        </div> -->
+        </br>
+        <div class="row">
+            <p>
+                <a href="create.php" class="btn btn-success">Adicionar</a>
+            </p>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                    include 'banco.php';
+                    $pdo = Banco::conectar();
+                    $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                    foreach ($pdo->query($sql) as $row) {
+                        if ($row['status'] != 'Entregue') {
                             echo '<tr>';
-			                echo '<th scope="row">'. $row['id'] . '</th>';
-                            echo '<td>'. $row['local'] . '</td>';
-                            echo '<td>'. $row['setor'] . '</td>';
-                            echo '<td>'. $row['entrada'] . '</td>';
-                            echo '<td>'. $row['status'] . '</td>';
-                            echo '<td>'. $row['chapa'] . '</td>';
-                            echo '<td>'. $row['chamado'] . '</td>';
+                            echo '<th scope="row">' . $row['id'] . '</th>';
+                            echo '<td>' . $row['local'] . '</td>';
+                            echo '<td>' . $row['setor'] . '</td>';
+                            echo '<td>' . $row['entrada'] . '</td>';
+                            echo '<td>' . $row['status'] . '</td>';
+                            echo '<td>' . $row['chapa'] . '</td>';
+                            echo '<td>' . $row['chamado'] . '</td>';
                             echo '<td width=350>';
-                            echo '<a class="btn btn-primary" href="read.php?id='.$row['id'].'">Info</a>';
+                            echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
                             echo ' ';
-                            echo '<a class="btn btn-warning" href="update.php?id='.$row['id'].'">Atualizar</a>';
+                            echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
                             echo ' ';
-                            echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Excluir</a>';
+                            echo '<a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '">Excluir</a>';
                             echo ' ';
-                            echo '<a class="btn btn-dark" href="imprimir.php?id='.$row['id'].'">Imprimir</a>';
+                            echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
                             echo '</td>';
                             echo '</tr>';
                         }
-                        Banco::desconectar();
-                        ?>
-                    </tbody>
-                </table>
-            </div>
-            <hr>          
+                    }
+                    Banco::desconectar();
+                    ?>
+                </tbody>
+            </table>
+        </div>
+        <hr>
+
+    </div>
+
+    <footer>
+        <div class="container">
+            <span class="badge badge-secondary">v 1.0.0 &copy; 2021 - Marcos A. R. T. dos Santos</span>
+
 
         </div>
-        
-        <footer>
-            <div class="container">
-            <span class="badge badge-secondary">v 1.0.0</span>
-    <p>&copy; 2021 - Marcos A. R. T. dos Santos</p>
-
-            
-                    </div>
-        </footer>
-    <script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.3.1.js"
+        integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
     <!-- Latest compiled and minified JavaScript -->
     <script src="../assets/js/bootstrap.min.js"></script>
 </body>
