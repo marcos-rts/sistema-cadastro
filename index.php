e77179166faf50fdc88fcc8a027db51f76b1af9c
alexandre824 <50344569+alexandre824@users.noreply.github.com>
2022-01-26 12:26:22 -0300

    Troca de tela de logins, com lembrança de logon

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
+<?php
+
+// A sessão precisa ser iniciada em cada página diferente
+if (!isset($_SESSION)) session_start();
+
+// Verifica se não há a variável da sessão que identifica o usuário
+if (!isset($_SESSION['UsuarioID'])) {
+    // Destrói a sessão por segurança
+    session_destroy();
+}else {
+    header("Location: sistem/index.php");
+}
+
+?>
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
     <head>
         <title>Login 10</title>
         <meta charset="utf-8">
