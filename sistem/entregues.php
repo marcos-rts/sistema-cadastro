ea47b2ba9fab4e673786b85457c3a633cde622f1
alexandre824 <50344569+alexandre824@users.noreply.github.com>
2022-01-27 16:18:55 -0300

    Config de Niveis

--- sistem/entregues.php
+++ sistem/entregues.php
@@ -48,10 +48,15 @@ if (!isset($_SESSION['UsuarioID'])) {
                             </li>
                             <li class="nav-item">
                                 <a href="../logout.php" class="nav-link">Logout</a>
-                            </li>
+                                <?php
+                            if ($_SESSION['UsuarioNivel'] == '5') {
+                            ?>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">Configurações gerais</a>
                             </li>
+                            <?php
+                            }
+                            ?>
                         </ul>
                     </div>
                 </div>
@@ -72,10 +77,11 @@ if (!isset($_SESSION['UsuarioID'])) {
 
         </div> -->
         </br>
+        <!-- config de nivel 1 -->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '1') {
+        ?>
         <div class="row">
-            <p>
-                <a href="create.php" class="btn btn-success">Adicionar</a>
-            </p>
             <br>
             <table class="table table-striped">
                 <thead>
@@ -93,32 +99,204 @@ if (!isset($_SESSION['UsuarioID'])) {
                 </thead>
                 <tbody>
                     <?php
-                    include 'banco.php';
-                    $pdo = Banco::conectar();
-                    $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
 
-                    foreach ($pdo->query($sql) as $row) {
-                        if ($row['status'] == 'Entregue') {
-                            echo '<tr>';
-                            echo '<th scope="row">' . $row['id'] . '</th>';
-                            echo '<td>' . $row['local'] . '</td>';
-                            echo '<td>' . $row['setor'] . '</td>';
-                            echo '<td>' . $row['entrada'] . '</td>';
-                            echo '<td>' . $row['status'] . '</td>';
-                            echo '<td>' . $row['chapa'] . '</td>';
-                            echo '<td>' . $row['chamado'] . '</td>';
-                            echo '<td width=100>';
-                            echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
-                            echo ' ';
-                            echo '</td>';
-                            echo '</tr>';
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] == 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=200>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
                         }
-                    }
-                    Banco::desconectar();
-                    ?>
+                        Banco::desconectar();
+                        ?>
                 </tbody>
             </table>
         </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 1 -->
+
+        <!-- config de nivel 2 -->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '2') {
+        ?>
+        <div class="row">
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] == 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=200>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                        Banco::desconectar();
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 2 -->
+
+        <!-- config de nivel 3 e 4-->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '3' || $_SESSION['UsuarioNivel'] == '4') {
+        ?>
+        <div class="row">
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] == 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=300>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                        Banco::desconectar();
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 3 e 4-->
+
+        <!-- config de nivel 5-->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '5') {
+        ?>
+        <div class="row">
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] == 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=350>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '">Excluir</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                        Banco::desconectar();
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 5-->
+
         <hr>
 
     </div>
--- sistem/index.php
+++ sistem/index.php
@@ -49,9 +49,17 @@ if (!isset($_SESSION['UsuarioID'])) {
                             <li class="nav-item">
                                 <a href="../logout.php" class="nav-link">Logout</a>
                             </li>
+                            <?php
+                            if ($_SESSION['UsuarioNivel'] == '5') {
+                            ?>
                             <li class="nav-item">
                                 <a href="#" class="nav-link">Configurações gerais</a>
                             </li>
+                            <?php
+                            }
+                            ?>
+
+
                         </ul>
                     </div>
                 </div>
@@ -61,10 +69,10 @@ if (!isset($_SESSION['UsuarioID'])) {
         </div>
     </header>
     <div class="container">
-        <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
+        <p>Conectado como <?php echo $_SESSION['UsuarioNome'], ' nivel: ', $_SESSION['UsuarioNivel']; ?> </p>
 
         <!-- <div class="jumbotron">
-            <p>Conectado como <?php echo $_SESSION['UsuarioNome']; ?> </p>
+            <p>Conectado como <?php echo $_SESSION['UsuarioNome'], 'nivel: ', $_SESSION['Us']; ?> </p>
 
             <div class="row">
                 <h2>SAA - Controle de Maquinas</h2>
@@ -72,6 +80,10 @@ if (!isset($_SESSION['UsuarioID'])) {
 
         </div> -->
         </br>
+        <!-- config de nivel 1 -->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '1') {
+        ?>
         <div class="row">
             <p>
                 <a href="create.php" class="btn btn-success">Adicionar</a>
@@ -93,37 +105,160 @@ if (!isset($_SESSION['UsuarioID'])) {
                 </thead>
                 <tbody>
                     <?php
-                    include 'banco.php';
-                    $pdo = Banco::conectar();
-                    $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
 
-                    foreach ($pdo->query($sql) as $row) {
-                        if ($row['status'] != 'Entregue') {
-                            echo '<tr>';
-                            echo '<th scope="row">' . $row['id'] . '</th>';
-                            echo '<td>' . $row['local'] . '</td>';
-                            echo '<td>' . $row['setor'] . '</td>';
-                            echo '<td>' . $row['entrada'] . '</td>';
-                            echo '<td>' . $row['status'] . '</td>';
-                            echo '<td>' . $row['chapa'] . '</td>';
-                            echo '<td>' . $row['chamado'] . '</td>';
-                            echo '<td width=350>';
-                            echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
-                            echo ' ';
-                            echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
-                            echo ' ';
-                            echo '<a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '">Excluir</a>';
-                            echo ' ';
-                            echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
-                            echo '</td>';
-                            echo '</tr>';
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] != 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=200>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
                         }
-                    }
-                    Banco::desconectar();
-                    ?>
+                        Banco::desconectar();
+                        ?>
                 </tbody>
             </table>
         </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 1 -->
+
+        <!-- config de nivel 2, 3 e 4-->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '2' || $_SESSION['UsuarioNivel'] == '3' || $_SESSION['UsuarioNivel'] == '4') {
+        ?>
+        <div class="row">
+            <p>
+                <a href="create.php" class="btn btn-success">Adicionar</a>
+            </p>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] != 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=300>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                        Banco::desconectar();
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 2, 3 e 4-->
+
+        <!-- config de nivel 5-->
+        <?php
+        if ($_SESSION['UsuarioNivel'] == '5') {
+        ?>
+        <div class="row">
+            <p>
+                <a href="create.php" class="btn btn-success">Adicionar</a>
+            </p>
+            <br>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Id</th>
+                        <!-- <th scope="col">Nome</th> -->
+                        <th scope="col">Local</th>
+                        <th scope="col">Setor</th>
+                        <th scope="col">Entrada</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Chapa</th>
+                        <th scope="col">Chamado</th>
+                        <th scope="col">Ação</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
+                        include 'banco.php';
+                        $pdo = Banco::conectar();
+                        $sql = 'SELECT * FROM maquina ORDER BY id DESC';
+
+                        foreach ($pdo->query($sql) as $row) {
+                            if ($row['status'] != 'Entregue') {
+                                echo '<tr>';
+                                echo '<th scope="row">' . $row['id'] . '</th>';
+                                echo '<td>' . $row['local'] . '</td>';
+                                echo '<td>' . $row['setor'] . '</td>';
+                                echo '<td>' . $row['entrada'] . '</td>';
+                                echo '<td>' . $row['status'] . '</td>';
+                                echo '<td>' . $row['chapa'] . '</td>';
+                                echo '<td>' . $row['chamado'] . '</td>';
+                                echo '<td width=350>';
+                                echo '<a class="btn btn-primary" href="read.php?id=' . $row['id'] . '">Info</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-warning" href="update.php?id=' . $row['id'] . '">Atualizar</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-danger" href="delete.php?id=' . $row['id'] . '">Excluir</a>';
+                                echo ' ';
+                                echo '<a class="btn btn-dark" href="imprimir.php?id=' . $row['id'] . '">Imprimir</a>';
+                                echo '</td>';
+                                echo '</tr>';
+                            }
+                        }
+                        Banco::desconectar();
+                        ?>
+                </tbody>
+            </table>
+        </div>
+        <?php
+        }
+        ?>
+        <!-- fim config de nivel 5-->
+
+
+
         <hr>
 
     </div>
